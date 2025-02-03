a9258576a394ba7eb681b64e2f2049a4ed7506fe
Toshinori Sumida <sumida@vectis.co.jp>
2025-02-03 13:56:07 +0900

    Auth.js カスタムサンプル修正漏れ

--- auth.ts
+++ auth.ts
@@ -1,20 +1,28 @@
-import NextAuth from "next-auth"
-import Keycloak from "@auth/core/providers/keycloak";
+import { authConfig } from "@/config/authConfig";
+import NextAuth from "next-auth";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Keycloak],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.refreshToken = account.refresh_token;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.accessToken = token.accessToken;
-      session.refreshToken = token.refreshToken;
-      return session;
-    },
-  },
-});
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth(authConfig);
+
+// sample...
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [Keycloak],
+//   callbacks: {
+//     async jwt({ token, account }) {
+//       if (account) {
+//         token.accessToken = account.access_token;
+//         token.refreshToken = account.refresh_token;
+//       }
+//       return token;
+//     },
+//     async session({ session, token }) {
+//       session.accessToken = token.accessToken;
+//       session.refreshToken = token.refreshToken;
+//       return session;
+//     },
+//   },
+// });
