74368c709c8fd68b7c92635c0535f75b193407b7
Toshinori Sumida <sumida@vectis.co.jp>
2025-02-05 10:18:08 +0900

    Redis設定リファクタリング

--- utils/auth.ts
+++ utils/auth.ts
@@ -9,7 +9,6 @@ async function fetchTokens(
   const clientSecret = process.env.CLIENT_SECRET!;
   const redirectUri = process.env.REDIRECT_URI!;
 
-  console.log('test', tokenEndpoint, clientId, clientSecret, redirectUri)
   const response = await fetch(tokenEndpoint, {
     method: "POST",
     headers: {
